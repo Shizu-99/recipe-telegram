b7f615d1da1ad6ad315654df0d736aa335f9bb9c
Shizu-99 <flew1440@gmail.com>
2025-09-19 20:43:31 +1200

    Creates data path folders and opens sqlite database.

--- main.go
+++ main.go
@@ -2,14 +2,34 @@ package main
 
 import (
 	"context"
+	"os"
 	"os/signal"
+	"path/filepath"
 	"syscall"
 
-	"github.com/Shizu-99/recipe-telegram/api"
 	"github.com/gin-contrib/graceful"
+
+	"github.com/Shizu-99/recipe-telegram/api"
+	"github.com/Shizu-99/recipe-telegram/database"
+)
+
+var (
+	dataPath = filepath.Join(".", "files")
 )
 
 func main() {
+	if envDataPath := os.Getenv("RECIPES_DATA_PATH"); envDataPath != "" {
+		dataPath = envDataPath
+	}
+	if err := os.MkdirAll(dataPath, 0755); err != nil {
+		panic(err)
+	}
+
+	if err := database.OpenDatabase(filepath.Join(dataPath, "recipes.sqlite3")); err != nil {
+		panic(err)
+	}
+	defer database.CloseDatabase()
+
 	//Ensure that Gin exits cleanly using Graceful
 	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
 	defer stop()
