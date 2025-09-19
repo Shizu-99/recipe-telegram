3ab8b0daac01af5e19ea6704722d2a6a2d2a1487
Shizu-99 <flew1440@gmail.com>
2025-09-19 15:06:26 +1200

    Setup gin router using graceful

--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"context"
+	"os/signal"
+	"syscall"
 
+	"github.com/Shizu-99/recipe-telegram/api"
+	"github.com/gin-contrib/graceful"
+)
+
+func main() {
+	//Ensure that Gin exits cleanly using Graceful
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+	router, err := graceful.Default()
+	if err != nil {
+		panic(err)
+	}
+	defer router.Close()
+
+	router.GET("/", api.Home)
+
+	if err := router.RunWithContext(ctx); err != nil && err != context.Canceled {
+		panic(err)
+	}
 }
