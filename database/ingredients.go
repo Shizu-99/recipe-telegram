fa65d4a3a85ea244ae1849988906fbb6f8b4e10f
Shizu-99 <flew1440@gmail.com>
2025-09-19 21:21:04 +1200

    Added database functions to insert, get and delete ingredients from the database.

---
+++ database/ingredients.go
@@ -0,0 +1,48 @@
+package database
+
+import (
+	"fmt"
+
+	"github.com/Shizu-99/recipe-telegram/models"
+)
+
+func DBInsertIngredient(ingredient *models.Ingredient) error {
+	_, err := db.NamedExec(`INSERT INTO ingredients (name, cost) VALUES (:name, :cost)`, ingredient)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func DBGetIngredientByName(name string) (*models.Ingredient, error) {
+	ingredient := &models.Ingredient{}
+	err := db.Get(ingredient, `SELECT * FROM ingredients WHERE name=$1`, name)
+	if err != nil {
+		return nil, err
+	}
+	return ingredient, nil
+}
+
+func DBGetAllIngredients() ([]models.Ingredient, error) {
+	ingredients := []models.Ingredient{}
+	err := db.Select(&ingredients, `SELECT * FROM ingredients`)
+	if err != nil {
+		return nil, err
+	}
+	return ingredients, nil
+}
+
+func DBRemoveIngredientByName(name string) error {
+	res, err := db.Exec(`DELETE FROM ingredients WHERE name=$1`, name)
+	if err != nil {
+		return err
+	}
+	rows, err := res.RowsAffected()
+	if err != nil {
+		return err
+	}
+	if rows != 1 {
+		return fmt.Errorf("expected to affect 1 row. affected %d", rows)
+	}
+	return nil
+}
