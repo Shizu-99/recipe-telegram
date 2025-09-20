4297ad6662219b68378552dd46de7378b413af65
Shizu-99 <flew1440@gmail.com>
2025-09-20 15:56:52 +1200

    Removed id from ingredient model.

--- models/models.go
+++ models/models.go
@@ -1,7 +1,6 @@
 package models
 
 type Ingredient struct {
-	ID   int     `db:"ingredient_id" json:"ingredient_id"`
 	Name string  `db:"name" json:"name"`
 	Type string  `db:"type" json:"type"`
 	Cost float64 `db:"cost" json:"cost"`
