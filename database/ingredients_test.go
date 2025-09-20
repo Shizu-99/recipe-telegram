066272ca90954d53534a2ae2af862ad6cf48258f
Shizu-99 <flew1440@gmail.com>
2025-09-20 17:09:44 +1200

    Tests for all current DB ingredient functions

--- database/ingredients_test.go
+++ database/ingredients_test.go
@@ -1,6 +1,7 @@
 package database
 
 import (
+	"database/sql"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -68,6 +69,220 @@ func TestDBInsertIngredient(t *testing.T) {
 			}
 
 			assert.Equal(t, test.expectedDBEntries, len(entries))
+		})
+	}
+}
+
+func TestGetIngredientByName(t *testing.T) {
+	ingredients := []*models.Ingredient{
+		{
+			Name: "STHL Vodka",
+			Type: "Vodka",
+			Cost: 45.34,
+		},
+		{
+			Name: "Fakerson Vodka",
+			Type: "Vodka",
+			Cost: 32.0,
+		},
+		{
+			Name: "Apple",
+			Type: "Fruit",
+			Cost: 1.2,
+		},
+		{
+			Name: "Banana",
+			Type: "Fruit",
+			Cost: 2.45,
+		},
+	}
+	tests := []struct {
+		name               string
+		nameToGet          string
+		expectedIngredient *models.Ingredient
+		expectedErrMsg     string
+	}{
+		{
+			name:               "Successful Retrieval",
+			nameToGet:          "STHL Vodka",
+			expectedIngredient: ingredients[0],
+			expectedErrMsg:     "",
+		},
+		{
+			name:               "No Ingredient with Name",
+			nameToGet:          "Rambutan",
+			expectedIngredient: nil,
+			expectedErrMsg:     sql.ErrNoRows.Error(),
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			if err := OpenDatabase(":memory:"); err != nil {
+				panic(err)
+			}
+			defer CloseDatabase()
+
+			for _, ingredient := range ingredients {
+				err := DBInsertIngredient(ingredient)
+				if err != nil {
+					t.Errorf("Did not expect error when inserting %+v. Error: %s", ingredient, err)
+				}
+			}
+
+			actualIngredient, err := DBGetIngredientByName(test.nameToGet)
+
+			if test.expectedErrMsg != "" {
+				assert.Equal(t, test.expectedErrMsg, err.Error())
+			} else {
+				assert.Equal(t, test.expectedIngredient, actualIngredient)
+			}
+		})
+	}
+}
+
+func TestDBGetAllIngredients(t *testing.T) {
+	ingredients := []*models.Ingredient{
+		{
+			Name: "STHL Vodka",
+			Type: "Vodka",
+			Cost: 45.34,
+		},
+		{
+			Name: "Fakerson Vodka",
+			Type: "Vodka",
+			Cost: 32.0,
+		},
+		{
+			Name: "Apple",
+			Type: "Fruit",
+			Cost: 1.2,
+		},
+		{
+			Name: "Banana",
+			Type: "Fruit",
+			Cost: 2.45,
+		},
+	}
+	tests := []struct {
+		name                   string
+		expectedIngredientsNum int
+	}{
+		{
+			name:                   "Successful Retrieval",
+			expectedIngredientsNum: 4,
+		},
+		{
+			name:                   "No rows",
+			expectedIngredientsNum: 0,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			if err := OpenDatabase(":memory:"); err != nil {
+				panic(err)
+			}
+			defer CloseDatabase()
+
+			if test.name == "Successful Retrieval" {
+				for _, ingredient := range ingredients {
+					err := DBInsertIngredient(ingredient)
+					if err != nil {
+						t.Errorf("Did not expect error when inserting %+v. Error: %s", ingredient, err)
+					}
+				}
+			}
+
+			actualIngredients, err := DBGetAllIngredients()
+			if err != nil {
+				t.Errorf("Did not expect an error getting all ingredients. Error: %s", err)
+			}
+
+			assert.Equal(t, test.expectedIngredientsNum, len(actualIngredients))
+
+		})
+	}
+}
+
+func TestDBRemoveIngredientByName(t *testing.T) {
+	ingredients := []*models.Ingredient{
+		{
+			Name: "STHL Vodka",
+			Type: "Vodka",
+			Cost: 45.34,
+		},
+		{
+			Name: "Fakerson Vodka",
+			Type: "Vodka",
+			Cost: 32.0,
+		},
+		{
+			Name: "Apple",
+			Type: "Fruit",
+			Cost: 1.2,
+		},
+		{
+			Name: "Banana",
+			Type: "Fruit",
+			Cost: 2.45,
+		},
+	}
+	tests := []struct {
+		name                 string
+		nameToDelete         string
+		remainingIngredients []models.Ingredient
+		expectedErrMsg       string
+	}{
+		{
+			name:         "Successful Deleteion",
+			nameToDelete: "Apple",
+			remainingIngredients: []models.Ingredient{
+				*ingredients[0],
+				*ingredients[1],
+				*ingredients[3],
+			},
+			expectedErrMsg: "",
+		},
+		{
+			name:         "No Ingredient with Name",
+			nameToDelete: "Rambutan",
+			remainingIngredients: []models.Ingredient{
+				*ingredients[0],
+				*ingredients[1],
+				*ingredients[2],
+				*ingredients[3],
+			},
+			expectedErrMsg: "expected to affect 1 row. affected 0",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			if err := OpenDatabase(":memory:"); err != nil {
+				panic(err)
+			}
+			defer CloseDatabase()
+
+			for _, ingredient := range ingredients {
+				err := DBInsertIngredient(ingredient)
+				if err != nil {
+					t.Errorf("Did not expect error when inserting %+v. Error: %s", ingredient, err)
+				}
+			}
+
+			err := DBRemoveIngredientByName(test.nameToDelete)
+
+			if test.expectedErrMsg != "" {
+				assert.Equal(t, test.expectedErrMsg, err.Error())
+			}
+
+			actualIngredients, err := DBGetAllIngredients()
+			if err != nil {
+				panic(err)
+			}
+
+			assert.Equal(t, test.remainingIngredients, actualIngredients)
 		})
 	}
 }
