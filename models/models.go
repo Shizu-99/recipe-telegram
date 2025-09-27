5aca3f30b632ad0bc74da9d57a374c93a317e971
Shizu-99 <flew1440@gmail.com>
2025-09-27 17:01:30 +1200

    Add recipe struct

--- models/models.go
+++ models/models.go
@@ -1,7 +1,17 @@
 package models
 
+import "image"
+
 type Ingredient struct {
 	Name string  `db:"name" json:"name"`
 	Type string  `db:"type" json:"type"`
 	Cost float64 `db:"cost" json:"cost"`
+}
+
+type Recipe struct {
+	Name        string       `db:"name" json:"name"`
+	Ingredients []Ingredient `db:"ingredients" json:"ingredients"`
+	Method      string       `db:"method" json:"method"`
+	Source      string       `db:"source" json:"source"`
+	RecipeImage image.Image  `db:"recipe_image" json:"recipe_image"`
 }
