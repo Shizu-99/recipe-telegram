4620793f11440a3d157ba3d3a9792e26b52a6b30
Shizu-99 <flew1440@gmail.com>
2025-09-27 17:16:50 +1200

    Remove ingredient struct

--- models/models.go
+++ models/models.go
@@ -2,16 +2,10 @@ package models
 
 import "image"
 
-type Ingredient struct {
-	Name string  `db:"name" json:"name"`
-	Type string  `db:"type" json:"type"`
-	Cost float64 `db:"cost" json:"cost"`
-}
-
 type Recipe struct {
-	Name        string       `db:"name" json:"name"`
-	Ingredients []Ingredient `db:"ingredients" json:"ingredients"`
-	Method      string       `db:"method" json:"method"`
-	Source      string       `db:"source" json:"source"`
-	RecipeImage image.Image  `db:"recipe_image" json:"recipe_image"`
+	Name        string      `db:"name" json:"name"`
+	Ingredients []string    `db:"ingredients" json:"ingredients"`
+	Method      string      `db:"method" json:"method"`
+	Source      string      `db:"source" json:"source"`
+	RecipeImage image.Image `db:"recipe_image" json:"recipe_image"`
 }
